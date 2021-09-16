a33eacc33e33f98b41ef71af2e2c58dedf2b8762
César Ortiz <cesar.ortiz@mail.escuelaing.edu.co>
2021-09-15 22:21:49 -0500

    implementando metodos de MongoWeatherService con sus test

--- src/main/java/org/adaschool/tdd/service/MongoWeatherService.java
+++ src/main/java/org/adaschool/tdd/service/MongoWeatherService.java
@@ -1,47 +1,82 @@
 package org.adaschool.tdd.service;
 
 import org.adaschool.tdd.controller.weather.dto.WeatherReportDto;
+import org.adaschool.tdd.exception.WeatherReportNotFoundException;
 import org.adaschool.tdd.repository.WeatherReportRepository;
 import org.adaschool.tdd.repository.document.GeoLocation;
 import org.adaschool.tdd.repository.document.WeatherReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class MongoWeatherService
-    implements WeatherService
-{
+public class MongoWeatherService implements WeatherService {
 
     private final WeatherReportRepository repository;
 
-    public MongoWeatherService( @Autowired WeatherReportRepository repository )
-    {
+    public MongoWeatherService( @Autowired WeatherReportRepository repository ) {
         this.repository = repository;
     }
 
     @Override
     public WeatherReport report( WeatherReportDto weatherReportDto )
     {
-        return null;
+        WeatherReport report = new WeatherReport(weatherReportDto);
+        return repository.save(report);
     }
 
     @Override
     public WeatherReport findById( String id )
     {
-        throw new RuntimeException( "Implement this method" );
+        Optional<WeatherReport> optional = repository.findById( id );
+        if ( optional.isPresent() ) {
+            return optional.get();
+        }
+        else {
+            throw new WeatherReportNotFoundException();
+        }
     }
 
     @Override
     public List<WeatherReport> findNearLocation( GeoLocation geoLocation, float distanceRangeInMeters )
     {
-        return null;
+        List<WeatherReport> reports = repository.findAll();
+        List<WeatherReport> reportsFound = new ArrayList<>();
+        for(WeatherReport r : reports){
+            if(distance(r.getGeoLocation().getLat(),r.getGeoLocation().getLng(),geoLocation.getLat(),geoLocation.getLng()) <= distanceRangeInMeters){
+                reportsFound.add(r);
+            }
+        }
+        return reportsFound;
+    }
+
+    private double distance(double lat1, double lng1,double lat2, double lng2){
+        double earthRadius = 6371;
+        double dlon = resta(lng2,lng1);
+        double dlat = resta(lat2,lat1);
+        double sinlat = Math.sin(dlat / 2);
+        double sinlon = Math.sin(dlon / 2);
+        double a = (sinlat * sinlat) + Math.cos(lat1)*Math.cos(lat2)*(sinlon*sinlon);
+        double c = 2 * Math.asin (Math.min(1.0, Math.sqrt(a)));
+        return earthRadius * c * 1000;
+    }
+
+    public double resta(double num1, double num2){
+        return Math.toRadians(num1-num2);
     }
 
     @Override
-    public List<WeatherReport> findWeatherReportsByName( String reporter )
-    {
-        return null;
+    public List<WeatherReport> findWeatherReportsByName( String reporter ) {
+        List<WeatherReport> WeatherReports= repository.findAll();
+        List<WeatherReport> WeatherReportsFoundReporter = new ArrayList<>();
+        for(WeatherReport r : WeatherReports){
+            if(r.getReporter().equals(reporter)){
+                WeatherReportsFoundReporter.add(r);
+            }
+        }
+        return WeatherReportsFoundReporter;
     }
 }
--- src/test/java/org/adaschool/tdd/MongoWeatherServiceTest.java
+++ src/test/java/org/adaschool/tdd/MongoWeatherServiceTest.java
@@ -7,14 +7,13 @@ import org.adaschool.tdd.repository.document.GeoLocation;
 import org.adaschool.tdd.repository.document.WeatherReport;
 import org.adaschool.tdd.service.MongoWeatherService;
 import org.adaschool.tdd.service.WeatherService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -30,21 +29,20 @@ class MongoWeatherServiceTest
     @Mock
     WeatherReportRepository repository;
 
-    @BeforeAll()
-    public void setup()
-    {
+    @BeforeEach()
+    public void setup() {
+
         weatherService = new MongoWeatherService( repository );
     }
 
     @Test
-    void createWeatherReportCallsSaveOnRepository()
+    void weatherReportIdNotFoundTest()
     {
-        double lat = 4.7110;
-        double lng = 74.0721;
-        GeoLocation location = new GeoLocation( lat, lng );
-        WeatherReportDto weatherReportDto = new WeatherReportDto( location, 35f, 22f, "tester", new Date() );
-        weatherService.report( weatherReportDto );
-        verify( repository ).save( any( WeatherReport.class ) );
+        String weatherReportId = "dsawe1fasdasdoooq123";
+        when( repository.findById( weatherReportId ) ).thenReturn( Optional.empty() );
+        Assertions.assertThrows( WeatherReportNotFoundException.class, () -> {
+            weatherService.findById( weatherReportId );
+        } );
     }
 
     @Test
@@ -61,13 +59,42 @@ class MongoWeatherServiceTest
     }
 
     @Test
-    void weatherReportIdNotFoundTest()
+    void createWeatherReportCallsSaveOnRepository()
     {
-        String weatherReportId = "dsawe1fasdasdoooq123";
-        when( repository.findById( weatherReportId ) ).thenReturn( Optional.empty() );
-        Assertions.assertThrows( WeatherReportNotFoundException.class, () -> {
-            weatherService.findById( weatherReportId );
-        } );
+        double lat = 4.7110;
+        double lng = 74.0721;
+        GeoLocation location = new GeoLocation( lat, lng );
+        WeatherReportDto weatherReportDto = new WeatherReportDto( location, 35f, 22f, "tester", new Date() );
+        weatherService.report( weatherReportDto );
+        verify( repository ).save( any( WeatherReport.class ) );
     }
 
+    @Test
+    void findNearLocation(){
+        List<WeatherReport> reports = new ArrayList<>();
+        GeoLocation location1 = new GeoLocation( 0.0, 75.0 );
+        GeoLocation location2 = new GeoLocation( 0.0, 75.0 );
+        GeoLocation location3 = new GeoLocation( 0.0, 5.0 );
+        reports.add(new WeatherReport( location1, 35f, 22f, "tester1", new Date() ));
+        reports.add(new WeatherReport( location2, 40f, 12f, "tester2", new Date() ));
+        reports.add(new WeatherReport( location3, 12f, 24f, "tester3", new Date() ));
+        GeoLocation locationTest = new GeoLocation( 0.0, 75.0 );
+        when( repository.findAll()).thenReturn(reports);
+        List<WeatherReport> foundNearLocation = weatherService.findNearLocation(locationTest, 1000);
+        Assertions.assertEquals( 2, foundNearLocation.size() );
+    }
+
+    @Test
+    void findWeatherReportsByName(){
+        List<WeatherReport> reports = new ArrayList<>();
+        GeoLocation location1 = new GeoLocation( 0.0, 75.0 );
+        GeoLocation location2 = new GeoLocation( 0.0, 75.0 );
+        GeoLocation location3 = new GeoLocation( 0.0, 5.0 );
+        reports.add(new WeatherReport( location1, 35f, 22f, "tester3", new Date() ));
+        reports.add(new WeatherReport( location2, 40f, 12f, "tester", new Date() ));
+        reports.add(new WeatherReport( location3, 12f, 24f, "tester4", new Date() ));
+        when( repository.findAll()).thenReturn(reports);
+        List<WeatherReport> foundWeatherReportsByName = weatherService.findWeatherReportsByName("tester3");
+        Assertions.assertEquals( 1, foundWeatherReportsByName.size() );
+    }
 }
